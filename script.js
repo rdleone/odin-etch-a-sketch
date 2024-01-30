a31c1d790cf3d563afe81848ffac9c2d52929c1f
Robert Leone <71949994+rdleone@users.noreply.github.com>
2024-01-29 18:07:07 -0600

    Color and fade modes

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const COLOR_COMBINATIONS = 16777216;
+
+/**
+ * Read the selected drawing mode
+ * @returns {string} normal, color, or fade
+ */
+function getMode() {
+    let mode;
+    const modeInputs = document.querySelectorAll("input[name=mode]");
+    modeInputs.forEach((input) => {
+        if(input.checked) {
+            mode = input.id;
+        }
+    })
+    return mode;
+}
+
 /**
  * Generate a square grid of equally sized square cells.
  * @param {int} factor The number of cells in each row/col
@@ -20,16 +37,64 @@ function setGrid(factor) {
 
 /**
  * Enable drawing in each of the drawable cells.
+ * @param {string} mode One of the modes under 'Options'. It changes the behavior of the pen on the grid
  */
 function activateCells() {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
         cell.addEventListener("mouseover", (event) => {
-            if(event.target.style.backgroundColor !== "black") {
-                event.target.style.backgroundColor = "black";
-            }
+            drawCell(cell);
         })
     });
+}
+
+/**
+ * Apply a new background color to the cell that the user is hovering over.
+ * The new color depends on the current drawing mode.
+ * @param {Node} cell The cell that the user is currently hovering over
+ */
+function drawCell(cell) {
+    const mode = getMode();
+    // console.log(cell);
+    const currColor = cell.style.backgroundColor ? cell.style.backgroundColor : "rgb(255,255,255)";
+    switch(mode) {
+
+        case "color":
+            let randHex = (Math.floor(Math.random() * COLOR_COMBINATIONS)).toString(16);
+            // Prepend zeros if randHex isn't a 6-digit hex string
+            while(randHex.length < 6) {
+                randHex = "0" + randHex;
+            }
+            const randColor = "#" + randHex;
+            cell.style.backgroundColor = randColor;
+        break;
+
+        case "fade":
+            if(currColor !== "rgb(0, 0, 0)") {
+                // Extract ints from rgb string
+                let colorArr = currColor.slice(currColor.indexOf("(")+1 , currColor.indexOf(")")).split(",");
+                let newColor = "";
+                colorArr.forEach((colorInt) => {
+                    if(colorInt !== 0) {
+                        // Darken by 10%, 256 possible values => subtract 26
+                        let newColorInt = parseInt(colorInt) - 26;
+                        if(newColorInt < 0) newColorInt = 0;
+                        let newColorHex = newColorInt.toString(16);
+                        while(newColorHex.length < 2) {
+                            newColorHex = "0" + newColorHex;
+                        }
+                        newColor = newColor + newColorHex;
+                    }
+                })
+                cell.style.backgroundColor = "#" + newColor;
+            }
+        break;
+
+        default:
+            if(currColor !== "rgb(0, 0, 0)") {
+                cell.style.backgroundColor = "rgb(0, 0, 0)";
+            }
+    }
 }
 
 /**
